1341f053be4e27b7609ba54f8d1e4e6caf1e7d07
Maximo Preneste <maxipreneste@gmail.com>
2025-04-10 01:36:31 -0300

    parte 3.3:template de detalles

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from .models import Question
+
 
 # Create your views here.
 def index(request):
@@ -8,7 +9,11 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 def detail(request, question_id):
-    return HttpResponse("Viendo la pregunta %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
